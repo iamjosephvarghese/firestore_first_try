151d44cae52686b44d497bcef964bcd7032a09af
iamjosephvarghese <josephvarghese.rms@gmail.com>
2017-12-29 17:27:14 +0530

    firestore pushing data working

--- app/src/main/java/com/firestore/joseph/firestorefirsttry/MainActivity.java
+++ app/src/main/java/com/firestore/joseph/firestorefirsttry/MainActivity.java
@@ -1,7 +1,17 @@
 package com.firestore.joseph.firestorefirsttry;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String,Object> student = new HashMap<>();
+        student.put("firstName","J");
+        student.put("lastName","V");
+        student.put("age",20);
+
+
+        db.collection("students").add(student).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                Log.d("Document Reference Id",documentReference.getId());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Error",e.toString());
+            }
+        });
+
     }
 }
